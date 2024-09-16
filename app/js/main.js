52dbdf7f127d6329342df2f8ddec3939459017af
Andrés Felipe Torres <felipeandres254@users.noreply.github.com>
2024-09-17 09:03:17 +1000

    Firebase test

---
+++ app/js/main.js
@@ -0,0 +1,27 @@
+//
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.13.1/firebase-app.js'
+import { getAuth, signInWithPopup, GoogleAuthProvider } from 'https://www.gstatic.com/firebasejs/10.13.1/firebase-auth.js'
+
+const API_URL = 'https://app-74ijiatkua-uc.a.run.app'
+const app = initializeApp({
+  authDomain: 'felipeandres254.github.io',
+  apiKey: 'AIzaSyBmrvd67uft_jBntHOvhij49NAudCxxcAI',
+  // projectId: 'app-514b35c713e28fcb31fc',
+  // appId: "APP_ID",
+  // For Firebase JavaScript SDK v7.20.0 and later, `measurementId` is an optional field
+  // measurementId: "G-MEASUREMENT_ID",
+})
+
+document.addEventListener('DOMContentLoaded', async () => {
+
+signInWithPopup(getAuth(), new GoogleAuthProvider())
+  .then((result) => {
+    const credential = GoogleAuthProvider.credentialFromResult(result)
+    console.log('result', result)
+    console.log('credential', credential)
+  })
+  .catch((error) => {
+    console.error('error', error)
+  })
+
+})
